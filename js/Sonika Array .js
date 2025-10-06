0abb62eccb3079ed884555bc9ab256b58966dd02
sonika45 <sonikakc985@gmail.com>
2025-10-06 23:07:50 +0545

    first commit

---
+++ js/Sonika Array .js
@@ -0,0 +1,6 @@
+let array=["apple","banana","mango"];
+console.log(array);
+array.push("grape");
+console.log(array);
+array.pop();
+console.log(array);
--- js/hello.js
+++ js/hello.js
@@ -1,3 +1,10 @@
 // Hello World in JavaScript
 // Author: Your Name 
-console.log('Hello, Hacktoberfest from JavaScript!');
+let name = "sonika kc";
+let age = 21;
+console.log(`My name is ${name} and I am ${age} years old.`);
+// This code prints a greeting message to the console.
+let array =["apple","banana","mango"];
+console.log(array);
+array.push("grape");
+console.log(array);
